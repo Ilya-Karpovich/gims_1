50e8d36cda10199262c478a3c1ecbaaf0087bf67
Илья <110288384+Ilya-Karpovich@users.noreply.github.com>
2024-03-02 17:59:20 +0300

    LinearMethods

--- gims_1/RefImageClass/LinearKontur.cs
+++ gims_1/RefImageClass/LinearKontur.cs
@@ -10,25 +10,25 @@ public class LinearKontur : Kontur
     private int kontrast;
 
     //Курсовые маски
-    public Mask north=new Mask("Курсовая маска (Север)", new int[3, 3] { {1, 1, 1}, { 1, -2, 1 }, { -1, -1, -1 } });
-    public Mask northEast = new Mask("Курсовая маска (Северо-Запад)",new int[3, 3] { { 1, 1, 1 }, { -1, -2, 1 }, { -1, -1, 1 } });
-    public Mask east = new Mask("Курсовая маска (Восток)",new int[3, 3] { { -1, 1, 1 }, { -1, -2, 1 }, { -1, 1, 1 } });
-    public Mask southEast = new Mask("Курсовая маска (Юго-Восток)", new int[3, 3] { { -1, -1, 1 }, { -1, -2, 1 }, { 1, 1, 1 } });
-    public Mask south =new Mask ("Курсовая маска (Юг)",new int[3, 3] { { -1, -1, -1 }, { 1, -2, 1 }, { 1, 1, 1 } });
-    public Mask southWest = new Mask("Курсовая маска (Юго-Запад)",new int[3, 3] { { 1, -1, -1 }, { 1, -2, -1 }, { 1, 1, 1 } });
-    public Mask west = new Mask("Курсовая маска (Запад)",new int[3, 3] { { 1, 1, -1 }, { 1, -2, -1 }, { 1, 1, -1 } });
-    public Mask northWest =new Mask("Курсовая маска (Северо-Запад)", new int[3, 3] { { 1, 1, 1 }, { 1, -2, -1 }, { 1, -1, -1 } });
+    private Mask north=new Mask("Курсовая маска (Север)", new int[3, 3] { {1, 1, 1}, { 1, -2, 1 }, { -1, -1, -1 } });
+    private Mask northEast = new Mask("Курсовая маска (Северо-Запад)",new int[3, 3] { { 1, 1, 1 }, { -1, -2, 1 }, { -1, -1, 1 } });
+    private Mask east = new Mask("Курсовая маска (Восток)",new int[3, 3] { { -1, 1, 1 }, { -1, -2, 1 }, { -1, 1, 1 } });
+    private Mask southEast = new Mask("Курсовая маска (Юго-Восток)", new int[3, 3] { { -1, -1, 1 }, { -1, -2, 1 }, { 1, 1, 1 } });
+    private Mask south =new Mask ("Курсовая маска (Юг)",new int[3, 3] { { -1, -1, -1 }, { 1, -2, 1 }, { 1, 1, 1 } });
+    private Mask southWest = new Mask("Курсовая маска (Юго-Запад)",new int[3, 3] { { 1, -1, -1 }, { 1, -2, -1 }, { 1, 1, 1 } });
+    private Mask west = new Mask("Курсовая маска (Запад)",new int[3, 3] { { 1, 1, -1 }, { 1, -2, -1 }, { 1, 1, -1 } });
+    private Mask northWest =new Mask("Курсовая маска (Северо-Запад)", new int[3, 3] { { 1, 1, 1 }, { 1, -2, -1 }, { 1, -1, -1 } });
 
     //Операторы Лапласа
-    public Mask laplasH13 = new Mask ("Оператор Лапласа (Н13)", new int[3, 3] { {0,-1,0}, {-1,4,-1}, {0,-1,0} });
-    public Mask laplasH14 = new Mask("Оператор Лапласа (Н14)", new int[3, 3] { { -1, -1, -1 }, { -1, 8, -1 }, { -1, -1, -1 } });
-    public Mask laplasH15 = new Mask("Оператор Лапласа (Н15)", new int[3, 3] { { 1, -2, 1 }, { -2, 4, -2 }, { 1, -2, 1 } });
+    private Mask laplasH13 = new Mask ("Оператор Лапласа (Н13)", new int[3, 3] { {0,-1,0}, {-1,4,-1}, {0,-1,0} });
+    private Mask laplasH14 = new Mask("Оператор Лапласа (Н14)", new int[3, 3] { { -1, -1, -1 }, { -1, 8, -1 }, { -1, -1, -1 } });
+    private Mask laplasH15 = new Mask("Оператор Лапласа (Н15)", new int[3, 3] { { 1, -2, 1 }, { -2, 4, -2 }, { 1, -2, 1 } });
 
     public void SetKontrast(int kontr)
     {
         this.kontrast = kontr;
     }
-    public System.Drawing.Color SetColor(int px, int kontr)
+    private System.Drawing.Color SetColor(int px, int kontr)
     {
         if (px > kontr)
         {
@@ -66,7 +66,7 @@ public class LinearKontur : Kontur
         return px[1, 1].grey;
     }
 
-    public Bitmap MakeKonturing( Mask mask)
+    private NamedBitmap MakeKonturing(Mask mask)
     {
         Bitmap bitmap=new Bitmap(bmp.Width, bmp.Height);
         for (int i = 0;i < bmp.Width;i++)
@@ -76,6 +76,27 @@ public class LinearKontur : Kontur
                 bitmap.SetPixel(i, j, SetColor(CalkulateKontur(i, j, mask), this.kontrast));
             }
         }
-        return bitmap;
+        NamedBitmap nBmp = new NamedBitmap(bitmap, mask.Name);
+        return nBmp;
     }
+
+    public List<NamedBitmap> GetKontures()
+    {
+        List<NamedBitmap > list = new List<NamedBitmap>();
+
+        list.Add(MakeKonturing(north));
+        list.Add(MakeKonturing(northEast));
+        list.Add(MakeKonturing(east));
+        list.Add(MakeKonturing(southEast));
+        list.Add(MakeKonturing(south));
+        list.Add(MakeKonturing(west));
+        list.Add(MakeKonturing(southWest));
+        list.Add(MakeKonturing(northWest));
+        list.Add(MakeKonturing(laplasH13));
+        list.Add(MakeKonturing(laplasH14));
+        list.Add(MakeKonturing(laplasH15));
+
+        return list;
+    }
+
 }
---
+++ gims_1/RefImageClass/NamedBitmap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class NamedBitmap
+{
+    Bitmap bitmap;
+    string name;
+    public NamedBitmap(Bitmap bitmap, string name)
+    {
+        this.bitmap = bitmap;
+        this.name = name;
+    }
+}
